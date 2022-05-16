8fce4c3dc5e0dd3042c7d7c7c0d53137856523ff
Amiel Danao <70653874+amiel-danao@users.noreply.github.com>
2022-05-16 10:03:41 +0800

    change fireapp order

--- firebase/settings.py
+++ firebase/settings.py
@@ -36,13 +36,13 @@ ALLOWED_HOSTS = [
 
 INSTALLED_APPS = [
     'baton',
+    'fireapp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'fireapp',
+    'django.contrib.staticfiles',    
     'baton.autodiscover',
 ]
 
