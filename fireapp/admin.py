12f049ce99a28b70f3b0b7dcc112c561ede1f6ad
Amiel Danao <70653874+amiel-danao@users.noreply.github.com>
2022-05-24 10:56:43 +0800

    filter_horizontal

--- fireapp/admin.py
+++ fireapp/admin.py
@@ -14,16 +14,14 @@ class CourseAdmin(admin.ModelAdmin):
     @admin.display(description='Department Name', ordering='department__name')
     def get_department_name(self, obj):
         return obj.course_department.name
-
-    """"
+    
     def formfield_for_manytomany(self, db_field, request=None, **kwargs):
-        if db_field.name == "subject_id":
+        if db_field.name == "subjects":
             qs = kwargs.get("queryset", db_field.remote_field.model.objects)
             # Avoid a major performance hit resolving permission names which
             # triggers a content_type load:
             kwargs["queryset"] = qs.select_related("content_type")
-        return super().formfield_for_manytomany(db_field, request=request, **kwargs)
-    """
+        return super().formfield_for_manytomany(db_field, request=request, **kwargs)    
 
 
 app_config = apps.get_app_config('fireapp') # Replace your_app_name it is just a placeholder
