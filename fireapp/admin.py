311cfcb1672076b069a00af2be7c0cf1cb8aa164
Amiel Danao <70653874+amiel-danao@users.noreply.github.com>
2022-05-24 10:54:47 +0800

    try multiple field in admin

--- fireapp/admin.py
+++ fireapp/admin.py
@@ -4,17 +4,26 @@ from django.contrib import admin
 from django.apps import apps
 from fireapp.models import Course, Subject
 
+@admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
     model = Course
     list_display = ('id', 'course_abbr', 'course_name', 'get_department_name', 'course_credit')
     list_editable = ['course_credit']
+    filter_horizontal = ('subjects',) 
 
     @admin.display(description='Department Name', ordering='department__name')
     def get_department_name(self, obj):
         return obj.course_department.name
 
-admin.site.register(Course, CourseAdmin)
-
+    """"
+    def formfield_for_manytomany(self, db_field, request=None, **kwargs):
+        if db_field.name == "subject_id":
+            qs = kwargs.get("queryset", db_field.remote_field.model.objects)
+            # Avoid a major performance hit resolving permission names which
+            # triggers a content_type load:
+            kwargs["queryset"] = qs.select_related("content_type")
+        return super().formfield_for_manytomany(db_field, request=request, **kwargs)
+    """
 
 
 app_config = apps.get_app_config('fireapp') # Replace your_app_name it is just a placeholder
