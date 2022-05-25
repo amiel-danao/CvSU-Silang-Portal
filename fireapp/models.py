a3fc38e0e5742057d641e34c5299148f0fd2871a
Amiel Danao <70653874+amiel-danao@users.noreply.github.com>
2022-05-25 08:41:08 +0800

    removed ugettext_lazy

--- fireapp/models.py
+++ fireapp/models.py
@@ -4,7 +4,7 @@ from django.dispatch import receiver
 from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.auth.models import AbstractUser
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from fireapp.managers import CustomUserManager
 
