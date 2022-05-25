18d788fe011bb5a9fcd2f15c0987b721d3d96dc9
Amiel Danao <70653874+amiel-danao@users.noreply.github.com>
2022-05-25 10:09:44 +0800

    added import timezone

--- fireapp/models.py
+++ fireapp/models.py
@@ -4,6 +4,7 @@ from django.dispatch import receiver
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
 
 from fireapp.managers import CustomUserManager
 
